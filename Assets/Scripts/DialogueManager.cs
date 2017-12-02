58ff335621f6e8c050118278a6776b85c96802c3
kdavidmitchell <kdavidmitchell@gmail.com>
2017-12-02 17:58:10 -0500

    Built third day night cycle

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -9,7 +9,8 @@ public class DialogueManager : MonoBehaviour
     public int currentAct;
 
     private Queue<string> sentences;
-    private int lineNumber = 0;
+    private bool paused = false;
+    public int lineNumber = 0;
 
     public Text dialogueText;
     public Animator animator;
@@ -17,7 +18,7 @@ public class DialogueManager : MonoBehaviour
     public RecordingManager recordingManager;
 
 	// Use this for initialization
-	void Start ()
+	void Awake ()
     {
         sentences = new Queue<string>();
         recordingManager = GameObject.Find("ActManager").GetComponent<RecordingManager>();	
@@ -26,9 +27,9 @@ public class DialogueManager : MonoBehaviour
     public void StartDialogue(LineManager lineManager)
     {
         animator.SetBool("isOpen", true);
+        lineNumber = 0;
 
         sentences.Clear();
-        lineNumber = 0;
 
         foreach (string sentence in lineManager.sentences)
         {
@@ -45,10 +46,6 @@ public class DialogueManager : MonoBehaviour
             //EndDialogue();
             return;
         }
-
-        CheckCurrentLineToEnableRecordingUI();
-        CheckCurrentLineToShowPortrait();
-        CheckCurrentLineToSwitchTitleCard();
 
         string sentence = sentences.Dequeue();
 
@@ -72,9 +69,26 @@ public class DialogueManager : MonoBehaviour
         {
             dialogueText.color = new Color(0.1f, 0.6f, 0.1f, 1f);
             dialogueText.fontStyle = FontStyle.Normal;
+        } else if (actManager.GetSpeaker(lineNumber, actManager.gameLM) == "Anne" && actManager.displayedTitleCard)
+        {
+            dialogueText.color = new Color(0.2f, 0.2f, 0.5f, 1f);
+            dialogueText.fontStyle = FontStyle.Normal;
+        } else if (actManager.GetSpeaker(lineNumber, actManager.gameLM) == "Douglas" && actManager.displayedTitleCard)
+        {
+            dialogueText.color = new Color(0.2f, 0.05f, 0.05f, 1f);
+            dialogueText.fontStyle = FontStyle.Normal;
         }
 
         dialogueText.text = sentence;
+
+        if (paused)
+        {
+            return;
+        }
+
+        CheckCurrentLineToEnableRecordingUI();
+        CheckCurrentLineToShowPortrait();
+        CheckCurrentLineToSwitchTitleCard();
         
         actManager.UpdateStrangeAndSuspicionValues(lineNumber, actManager.gameLM);
         
@@ -100,108 +114,249 @@ public class DialogueManager : MonoBehaviour
 
     public void CheckCurrentLineToEnableRecordingUI()
     {
-        if (lineNumber == 22 && currentAct == 1)
+        //Act 1 recording events
+        if (currentAct == 1)
         {
-            actManager.animatorRecordingUI.SetBool("isOpen", true);
-            actManager.EnableRecordingUI();
+            if (actManager.gameDM.lineNumber == 22)
+            {
+                actManager.animatorRecordingUI.SetBool("isOpen", true);
+                actManager.EnableRecordingUI();
+            }
+        }
+
+        //Act 2 recording events
+        if (currentAct == 2)
+        {
+            if (actManager.titleCardDM.lineNumber == 19)
+            {
+                actManager.animatorRecordingUI.SetBool("isOpen", true);
+                actManager.EnableRecordingUI();
+            }
+        }
+
+        //Act 3 recording event
+        if (currentAct == 3)
+        {
+            if (actManager.titleCardDM.lineNumber == 21)
+            {
+                actManager.animatorRecordingUI.SetBool("isOpen", true);
+                actManager.EnableRecordingUI();
+            }
         }
     }
 
     public void CheckCurrentLineToShowPortrait()
     {
-        if (lineNumber == 6 && currentAct == 1 && actManager.displayedTitleCard)
+
+        //Act 1 portrait events
+        if (currentAct == 1)
         {
-            actManager.animatorNPCPortrait.SetBool("isOpen", true);
+            if (actManager.gameDM.lineNumber == 6)
+            {
+                actManager.animatorNPCPortrait.SetBool("isOpen", true);
+            }
         }
 
-        if (lineNumber == 0 && currentAct == 2 && actManager.displayedTitleCard)
+        //Act 2 portrait events
+
+        if (currentAct == 2)
         {
-            actManager.UpdateBackground(1);
-            actManager.UpdatePortraitAndName(2);
-            actManager.animatorNPCPortrait.SetBool("isOpen", true);
+            if (actManager.gameDM.lineNumber == 0)
+            {
+                actManager.UpdateBackground(1);
+                actManager.UpdatePortraitAndName(2);
+            }
+
+            if (actManager.gameDM.lineNumber == 19)
+            {
+                actManager.animatorNPCPortrait.SetBool("isOpen", false);
+                actManager.UpdateBackground(2);
+                actManager.UpdatePortraitAndName(3);
+            }
+
+            if (actManager.gameDM.lineNumber == 21)
+            {
+                actManager.animatorNPCPortrait.SetBool("isOpen", true);
+            }
+
+            if (actManager.gameDM.lineNumber == 50)
+            {
+                actManager.animatorNPCPortrait.SetBool("isOpen", false);
+                actManager.UpdateBackground(4);
+            }
         }
 
-        if (lineNumber == 19 && currentAct == 2 && actManager.displayedTitleCard)
+        //Act 3 portrait events
+        if (currentAct == 3)
         {
-            actManager.animatorNPCPortrait.SetBool("isOpen", false);
-            actManager.UpdateBackground(2);
-            actManager.UpdatePortraitAndName(3);
-        }
+            if (actManager.gameDM.lineNumber == 0)
+            {
+                actManager.UpdateBackground(1);
+                actManager.UpdatePortraitAndName(2);
+            }
 
-        if (lineNumber == 21 && currentAct == 2 && actManager.displayedTitleCard)
-        {
-            actManager.animatorNPCPortrait.SetBool("isOpen", true);
-        }
+            if (actManager.titleCardDM.lineNumber == 10)
+            {
+                actManager.UpdateBackground(3);
+                actManager.UpdatePortraitAndName(4);
+            }
 
-        if (lineNumber == 50 && currentAct == 2 && actManager.displayedTitleCard)
-        {
-            actManager.animatorNPCPortrait.SetBool("isOpen", false);
+            if (actManager.gameDM.lineNumber == 46)
+            {
+                actManager.UpdateBackground(4);
+                actManager.UpdatePortraitAndName(5);
+            }
         }
     }
 
     public void CheckCurrentLineToSwitchTitleCard()
     {
-        if (lineNumber == 10 && currentAct == 1 && !actManager.displayedTitleCard)
+        //Act 1 switches
+
+        if (currentAct == 1)
         {
-            actManager.displayedTitleCard = true;
-            actManager.EnableGameCanvas();
-            actManager.gameDM.StartDialogue(actManager.gameLM);
-            actManager.DisableTitleCard();
+            if (actManager.titleCardDM.lineNumber == 10)
+            {
+                actManager.displayedTitleCard = true;
+                paused = true;
+                actManager.EnableGameCanvas();
+                actManager.gameDM.StartDialogue(actManager.gameLM);
+                paused = false;
+                actManager.DisableTitleCard();
+            }
         }
 
-        if (lineNumber == 8 && currentAct == 2 && !actManager.displayedTitleCard)
-        {
-            Debug.Log("Attempting to switch to game mode");
+        //Act 2 switches
 
-            actManager.displayedTitleCard = true;
-            DisplayNextSentence();
-            actManager.EnableGameCanvas();
-            actManager.gameDM.StartDialogue(actManager.gameLM);
-            actManager.DisableTitleCard();
+        if (currentAct == 2)
+        {
+            if (actManager.titleCardDM.lineNumber == 8)
+            {
+                actManager.displayedTitleCard = true;
+                paused = true;
+                DisplayNextSentence();
+                paused = false;
+                actManager.titleCardDM.lineNumber++;
+                actManager.EnableGameCanvas();
+                actManager.gameDM.StartDialogue(actManager.gameLM);
+                actManager.DisableTitleCard();
+                actManager.animatorNPCPortrait.SetBool("isOpen", true);
+            }
+
+            if (actManager.titleCardDM.lineNumber == 12)
+            {
+                actManager.displayedTitleCard = true;
+                paused = true;
+                DisplayNextSentence();
+                paused = false;
+                actManager.titleCardDM.lineNumber++;
+                actManager.EnableGameCanvas();
+                actManager.DisableTitleCard();
+            }
+
+            if (actManager.titleCardDM.lineNumber == 19)
+            {
+                actManager.displayedTitleCard = true;
+                paused = true;
+                DisplayNextSentence();
+                paused = false;
+                actManager.titleCardDM.lineNumber++;
+                actManager.EnableGameCanvas();
+                actManager.DisableTitleCard();
+            }
+
+            if (actManager.gameDM.lineNumber == 19)
+            {
+                actManager.displayedTitleCard = false;
+                paused = true;
+                DisplayNextSentence();
+                paused = false;
+                actManager.gameDM.lineNumber++;
+                actManager.EnableTitleCard();
+                actManager.UpdateTitleCardHeader(0);
+                actManager.titleCardDescription.color = Color.white;
+                actManager.DisableGameCanvas();
+            }
+
+            if (actManager.gameDM.lineNumber == 50)
+            {
+                actManager.displayedTitleCard = false;
+                paused = true;
+                DisplayNextSentence();
+                paused = false;
+                actManager.gameDM.lineNumber++;
+                actManager.EnableTitleCard();
+                actManager.UpdateTitleCardHeader(1);
+                actManager.titleCardDescription.color = Color.white;
+                actManager.DisableGameCanvas();
+            }
         }
 
-        if (lineNumber == 12 && currentAct == 2 && !actManager.displayedTitleCard)
+        //Act 3 switches
+        if (currentAct == 3)
         {
-            Debug.Log("Attempting to switch to game mode");
+            if (actManager.titleCardDM.lineNumber == 7)
+            {
+                actManager.displayedTitleCard = true;
+                paused = true;
+                DisplayNextSentence();
+                paused = false;
+                actManager.titleCardDM.lineNumber++;
+                actManager.EnableGameCanvas();
+                actManager.gameDM.StartDialogue(actManager.gameLM);
+                actManager.DisableTitleCard();
+                actManager.animatorNPCPortrait.SetBool("isOpen", true);
+            }
 
-            actManager.displayedTitleCard = true;
-            DisplayNextSentence();
-            actManager.EnableGameCanvas();
-            actManager.DisableTitleCard();
-        }
+            if (actManager.gameDM.lineNumber == 10)
+            {
+                actManager.displayedTitleCard = false;
+                paused = true;
+                DisplayNextSentence();
+                paused = false;
+                actManager.gameDM.lineNumber++;
+                actManager.EnableTitleCard();
+                actManager.UpdateTitleCardHeader(2);
+                actManager.titleCardDescription.color = Color.white;
+                actManager.titleCardDescription.fontStyle = FontStyle.Normal;
+                actManager.animatorNPCPortrait.SetBool("isOpen", false);
+                actManager.DisableGameCanvas();
+            }
 
-        if (lineNumber == 19 && currentAct == 2 && !actManager.displayedTitleCard)
-        {
-            Debug.Log("Attempting to switch to game mode");
+            if (actManager.titleCardDM.lineNumber == 13)
+            {
+                actManager.displayedTitleCard = true;
+                paused = true;
+                DisplayNextSentence();
+                paused = false;
+                actManager.titleCardDM.lineNumber++;
+                actManager.EnableGameCanvas();
+                actManager.DisableTitleCard();
+                actManager.animatorNPCPortrait.SetBool("isOpen", true);
+            }
 
-            actManager.displayedTitleCard = true;
-            actManager.EnableGameCanvas();
-            actManager.EnableRecordingUI();
-            actManager.DisableTitleCard();
-        }
+            if (actManager.gameDM.lineNumber == 46)
+            {
+                actManager.displayedTitleCard = false;
+                paused = true;
+                DisplayNextSentence();
+                paused = false;
+                actManager.gameDM.lineNumber++;
+                actManager.EnableTitleCard();
+                actManager.UpdateTitleCardHeader(3);
+                actManager.titleCardDescription.color = Color.white;
+                actManager.titleCardDescription.fontStyle = FontStyle.Normal;
+                actManager.animatorNPCPortrait.SetBool("isOpen", false);
+                actManager.DisableGameCanvas();
+            }
 
-        if (lineNumber == 19 && currentAct == 2 && actManager.displayedTitleCard)
-        {
-            Debug.Log("Attempting to switch to titlecard");
-
-            actManager.displayedTitleCard = false;
-            DisplayNextSentence();
-            actManager.EnableTitleCard();
-            actManager.UpdateTitleCardHeader(0);
-            actManager.titleCardDescription.color = Color.white;
-            actManager.DisableGameCanvas();
-        }
-
-        if (lineNumber == 50 && currentAct == 2 && actManager.displayedTitleCard)
-        {
-            Debug.Log("Attempting to switch to titlecard");
-
-            actManager.displayedTitleCard = false;
-            DisplayNextSentence();
-            actManager.EnableTitleCard();
-            actManager.UpdateTitleCardHeader(1);
-            actManager.titleCardDescription.color = Color.white;
-            actManager.DisableGameCanvas();
+            if (actManager.titleCardDM.lineNumber == 21)
+            {
+                actManager.displayedTitleCard = true;
+                actManager.EnableGameCanvas();
+                actManager.DisableTitleCard();
+                actManager.animatorNPCPortrait.SetBool("isOpen", true);
+            }
         }
     }
 }
